f114f2fbad2ddcc3cadfac1d381b634f3ed1e3fb
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-10-13 09:28:35 +0200

    Added server-udp

---
+++ udp/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <csignal>
+#include <netinet/in.h>
+
+int main() {
+    sockaddr_in addr = {
+            .sin_family = AF_INET,
+            .sin_port = htons(3000),
+            .sin_addr = {INADDR_ANY}
+    };
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    const int one = 1;
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+    int err = bind(sock, (sockaddr *) &addr, sizeof(addr));
+    if (err < 0) {
+        std::cerr << "Error binding socket" << std::endl;
+        return 1;
+    }
+    std::cout << "Listening on port 3000" << std::endl;
+    sockaddr_in client_addr{
+            .sin_family = AF_INET,
+            .sin_port = htons(3000),
+            .sin_addr = {INADDR_ANY}
+    };
+    socklen_t client_addr_len = sizeof(client_addr);
+    char buffer[100];
+    sleep(1);
+    while (true) {
+        ssize_t bytes_read = recvfrom(
+                sock,
+                buffer,
+                sizeof(buffer),
+                0,
+                (sockaddr *) &client_addr,
+                &client_addr_len
+        );
+        if (bytes_read == -1) {
+            perror("Error reading from socket");
+            return 1;
+        }
+        std::cout << "Received " << bytes_read << " bytes" << std::endl;
+        ssize_t bytes_written = sendto(
+                sock,
+                buffer,
+                (size_t) bytes_read,
+                0,
+                (sockaddr *) &client_addr,
+                client_addr_len
+        );
+        if (bytes_written == -1) {
+            perror("Error writing to socket");
+            return 1;
+        }
+        std::cout << "Sent " << bytes_written << " bytes" << std::endl;
+    }
+    return 0;
+}
